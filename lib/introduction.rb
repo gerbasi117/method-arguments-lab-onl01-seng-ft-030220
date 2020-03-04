51383941ba77c35861fd149c1bd9e18f7e327d28
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-04 04:48:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,5 +5,5 @@ def introduction(name)
     end
     
 def introduction_with_languages{name, languages}
-      puts "Hi, my name is #{name} and I am learning to program in #{languages}"
+      puts "Hi, my name is #{name} and I am learning to program in #{languages}."
     end
